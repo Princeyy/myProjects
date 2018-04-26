5788962ff4930b640531691251169cd8e9d23c03
Faddle Badiane <faddlebadiane@gmail.com>
2018-04-27 00:33:27 +0100

    update

--- Fibonacci/Fibonacci.java
+++ Fibonacci/Fibonacci.java
@@ -1,19 +1,17 @@
 public class Fibonacci {
 	
+	LogFileTest logFile = new LogFileTest();
 	private static long[] fib = new long[1000];	
 			
 	public static void main(String[] Args) {
-		fib[0] = 0;	fib[1] = 1;
-		for(int i=2;i<1000;i++) {
-			fib[i] = -1;
-			// -1 means fibonacci of that number is not known
-		}
-		
+		Fibonacci test = new Fibonacci();
+		fib[0] = 0;	fib[1] = 1;	
+		test.resetLogFile();
 		int n = Integer.parseInt(javax.swing.JOptionPane.showInputDialog("Enter fibonacci number to find:"));
-		System.out.println("Fibonacci Number of "+n+":\n"+fib(n,fib));
+		System.out.println("Fibonacci Number of "+n+":\n"+test.fib(n,fib));
 	}
 	
-	public static long fib(int n, long[] fib) {
+	public long fib(int n, long[] fib) {
 		//check if is known
 		if(fib[n] != -1) {
 			return fib[n];
@@ -22,16 +20,19 @@ public class Fibonacci {
 		if((fib[(n-1)] != -1) || (fib[(n-2)] != -1)) {
 			//both known
 			if((fib[(n-1)] != -1) && (fib[(n-2)] != -1)) {
+				logFile.write(fib);
 				return fib[(n-1)] + fib[(n-2)];
 			}
 			//one known
 			if(fib[(n-1)] != -1) {
 				fib[(n-2)] = fib((n-2),fib);	//recursively call and store value
+				logFile.write(fib);
 				return fib[(n-2)] + fib[(n-1)];
 			}
 			//other one known
 			if(fib[(n-2)] != -1) {
 				fib[(n-1)] = fib((n-1),fib);	//recursively call and store value
+				logFile.write(fib);
 				return fib[(n-1)] + fib[(n-2)];
 			}
 		}
@@ -39,8 +40,17 @@ public class Fibonacci {
 		else {
 			fib[n-2] = fib(n-2,fib);
 			fib[n-1] = fib(n-1,fib);
+			logFile.write(fib);
 			return fib[n-1] + fib[n-2];
 		}
 		return 0 ;	//error code
+	}
+	
+	public void resetLogFile() {
+		for(int i=2;i<1000;i++) {
+			fib[i] = -1;
+			// -1 means fibonacci of that number is not known
+		}
+		logFile.write(fib);
 	}
 }
